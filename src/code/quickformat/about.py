0ef35c3189ab8c084ae8418e1964af7a6d008c40
Renan Cakirerk <renan@cakirerk.org>
2011-04-10 04:17:28 +0000

    add license information

--- src/code/quickformat/about.py
+++ src/code/quickformat/about.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# Copyright (C) 2011 TUBITAK/BILGEM
+# Renan Çakırerk <renan at pardus.org.tr>
 #
-# Please read the COPYING file.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# (See COPYING)
 
 # PyKDE
 from PyKDE4.kdecore import KAboutData, ki18n
